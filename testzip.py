6804ca6f64dbb016a644ed8b4877a2e50899e066
Arseny Smirnov <arsenio.smirnov@gmail.com>
2021-07-09 05:29:26 +0000

    modified variable names in accordance to PEP8

--- testzip.py
+++ testzip.py
@@ -2,6 +2,20 @@
 
 import re
 from zipfile import ZipFile
+import os
+from read_reverse_order import read_reverse_order
+
+
+#logFile = open("job.log", "r")
+log_file = open("job.log", "r")
+#log_line = reverse_readline(log_file)
+#log_line = read_reverse_order(log_file)
+#print(logLine)
+#print(read_reverse_order("job.log"))
+
+# read file in reverse order
+#for line in read_reverse_order("job.log"):
+#  print(line)
 
 #z = ZipFile("2021-07-07T164414_VeeamBackupLogs.zip", "r")
 z = ZipFile("2021-07-07T161552_VeeamBackupLogs.zip", "r")
@@ -10,42 +24,66 @@ z = ZipFile("2021-07-07T161552_VeeamBackupLogs.zip", "r")
 #p = re.compile(".*Error.*", re.IGNORECASE)
 
 
-def matchFilePattern(filename):
+
+def match_file_pattern(filename):
   ## Patterns
   # String pattern
-  p = re.compile(".*(Error|Warning)  .*")
+  p = re.compile(".*(Error)  .*")
+  p2 = re.compile(".*Backup.Manager.*File version: \[[0-9].*\]")
+  #p = re.compile(".*(Error|Warning)  .*")
 
   # File name pattern
-  fileP = re.compile(".*Job.*log")
+  file_p = re.compile(".*Job.*log")
 
   ## Flags
-  isPreviousLineError = 0
-  linesWithoutError = 1
-  isFileClear = 1
+  is_previous_line_error = 0
+  lines_without_error = 1
+  is_file_clear = 1
 
-  mFileP = fileP.match(filename)
-  if mFileP:
-    isFileClear = 1
+  # Lists
+  reversed_out_list = []
+
+  m_file_p = file_p.match(filename)
+  if m_file_p:
+    is_file_clear = 1
+    reversed_out_list.append(str('File: ' + filename))
     print('File:', filename)
-    bytes = z.read(filename)
-    print('Size', round(len(bytes)/1024/1024,2), 'MB', '\n')
-    for line in bytes.decode('utf-8').split('\n'):
-      if not (isPreviousLineError or (linesWithoutError > 1)) :
+    bytes_archive = z.read(filename)
+    #print(bytes_archive.decode('utf-8'))
+    # Print file size
+    print('Size', round(len(bytes_archive)/1024/1024,2), 'MB', '\n')
+    string_list = bytes_archive.decode('utf-8').split('\n')
+    for line in string_list:
+    #for line in reversed(string_list):
+    #for line in bytes.decode('utf-8').split('\n'):
+    #print(type(bytes.decode('utf-8')))
+    #for line in read_reverse_order(bytes.decode('utf-8')):
+      if not (is_previous_line_error or (lines_without_error > 1)) :
         print('...')
+        reversed_out_list.append('...')
+        print(reversed_out_list)
+
       m = p.match(line)
+      #m2 = p2.match(line)
+      m2 = re.search('version: \[(.*)\], Assembly', line)
+      if m2:
+        #print(m2.match("File version: \[[0-9].*\]"))
+        print(m2.group(1))
+
       if m:
         print(m.group())
-        isPreviousLineError = 1
-        linesWithoutError = 0
-        isFileClear = 0
+        is_previous_line_error = 1
+        lines_without_error = 0
+        is_file_clear = 0
       else:
-        isPreviousLineError = 0
-        linesWithoutError += 1 
+        is_previous_line_error = 0
+        lines_without_error += 1 
      
-    if isFileClear:
+    if is_file_clear:
       print("No errors were found")
     print()
     print('-' * 30, '\n')
+    return(reversed_out_list)
 
 
 for filename in z.namelist():
@@ -53,4 +91,6 @@ for filename in z.namelist():
   #print(filename) 
 
   # match for file name pattern
-  matchFilePattern(filename)
+  match_file_pattern(filename)
+
+
